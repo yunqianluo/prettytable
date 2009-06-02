fb7bd303d006d15678695ef449bf90435d0990f7
luke.maurits <luke.maurits@0f58610c-415a-11de-9c03-5d6cfad8e937>
2009-06-02 11:29:47 +0000

    Branching 0.6

---
+++ branches/0.6/src/setup.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python
+from setuptools import setup
+from prettytable import __version__ as version
+
+setup(
+    name='prettytable',
+    version=version,
+    description='A simple Python library for easily displaying tabular data in a visually appealing ASCII table format',
+    author='Luke Maurits',
+    author_email='luke@maurits.id.au',
+    url='http://www.luke.maurits.id.au/software/prettytable',
+    license='http://www.luke.maurits.id.au/software/bsdlicense.txt',
+    py_modules=['prettytable']
+)
